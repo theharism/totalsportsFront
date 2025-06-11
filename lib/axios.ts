be322031cc64aa4a8b17d2de4aabd14d24c53b86
theharism <chharis9999@gmail.com>
2025-06-12 00:44:09 +0500

    fixed url

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Default config for the axios instance
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PROXY_URL || ' https://0b38-154-208-41-117.ngrok-free.app',
+  baseURL: process.env.NEXT_PROXY_URL || ' https://totalsportek.world',
   headers: {
     'Content-Type': 'application/json',
   },
