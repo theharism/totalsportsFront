723f6659489d3637c48bf420f17d2a516c312881
theharism <chharis9999@gmail.com>
2025-06-23 13:41:47 +0500

    img fix

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
         destination: 'https://dashgenius.space/api/v1/:path*',
       },
       {
-        source: '/uploads/:path*',
+        source: ':path*/uploads/:path*',
         destination: 'https://dashgenius.space/uploads/:path*',
       },
     ];
