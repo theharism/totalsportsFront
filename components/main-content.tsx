c6543c3325600d00387282d63e288671550647de
theharism <chharis9999@gmail.com>
2025-08-14 20:36:25 +0500

    open main page game in new tab

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -273,6 +273,8 @@ export default function Content({
                             <Link
                               href={`/game/${game.slug}`}
                               className="rounded-md p-2 hover:bg-[#333333]"
+                              target="_blank"
+                              rel="noopener noreferrer"
                             >
                               <ExternalLink className="h-5 w-5 text-gray-400" />
                             </Link>
