3e1c36cfc47f425e48538321e8ae535a67e73a5b
theharism <chharis9999@gmail.com>
2025-08-30 00:53:24 +0500

    removed adsterra + added monetag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
         <head>
           <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
           <meta name="admaven-placement" content="BqdU5qdaH" />
-          <script type='text/javascript' src='//pl27491559.profitableratecpm.com/07/e6/b7/07e6b712def0677913a7fd0e2007363c.js'></script>
         </head>
         <body>
             {children}
