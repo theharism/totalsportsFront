41c0b7e41dcf5a5e287275d19290df184518ac59
theharism <chharis9999@gmail.com>
2025-09-10 01:27:00 +0500

    updated blogs ui

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,15 @@
 export const revalidate = 60;
 
 import Header from "@/components/header"
-import LeftSidebar from "@/components/left-sidebar"
-import RightSidebar from "@/components/right-sidebar"
 import _ from "lodash"
-import { getTopCategories } from "@/queries/getTopCategoriesList"
-import { getTopTeams } from "@/queries/getTopTeamsList"
 import Footer from "@/components/footer"
 import { getAllBlogs } from "@/queries/getBlogsList"
 import { TotalsportekMetadata } from "@/constants/metadata";
+import Link from "next/link"
+import Image from "next/image"
+import { Blog } from "@/types/blog"
 
-export async function generateMetadata({ params }: { params: { slug: string } }) {
+export async function generateMetadata() {
   const page = TotalsportekMetadata.categories['blog'];
 
   if (!page) return {}
@@ -29,47 +28,44 @@ export async function generateMetadata({ params }: { params: { slug: string } })
   }
 }
 
-export default async function Home({ params }: { params: { slug: string } }) {
+export default async function Home() {
  
-  const categoriesData = await getTopCategories()
-  const teamsData = await getTopTeams()
   const blogsData = await getAllBlogs();
-
-  const categories = _.get(categoriesData, "data", [])
-  const teams = _.get(teamsData, "data", [])
   const blogs = _.get(blogsData, "data", [])
 
   return (
     <div className="min-h-screen bg-[#121212]">
       <Header />
-      <div className="container mx-auto grid grid-cols-1 gap-4 px-4 py-2 md:grid-cols-4 lg:grid-cols-12">
-        {/* Main Content - First on mobile, center on desktop */}
-        <div className="order-1 md:order-2 md:col-span-2 lg:col-span-6">
-          {/* <MainContent games={[]} isCategory={true} /> */}
-          {blogs.length > 0 && <div className="rounded-lg bg-[#1a1a1a] p-6 mt-1 hidden md:block">
-            {blogs?.map((blog: any) => (
-              <div key={blog._id} className="blog" dangerouslySetInnerHTML={{ __html: blog.content }} />
-            ))}
-          </div>}
-        </div>
-
-        {/* Left Sidebar - Second on mobile, first on desktop */}
-        <div className="order-2 md:order-1 md:col-span-1 lg:col-span-3">
-          <LeftSidebar categories={categories} teams={teams} />
-        </div>
-
-        {/* Right Sidebar - Third on mobile, third on desktop */}
-        <div className="order-3 md:order-3 md:col-span-1 lg:col-span-3">
-          <RightSidebar blogs={blogs} />
-        </div>
-
-        {/* Blog Content */}
-        <div className="order-4 md:order-4 md:col-span-2 lg:hidden">
-          {blogs.length > 0 && <div className="rounded-lg bg-[#1a1a1a] p-6 mt-1">
-            {blogs?.map((blog: any) => (
-              <div key={blog._id} className="blog" dangerouslySetInnerHTML={{ __html: blog.content }} />
-            ))}
-          </div>}
+      <div className="container mx-auto px-4 py-6 flex justify-center">
+        <div className="w-full max-w-5xl">
+          <div className="rounded-lg bg-[#1a1a1a] p-6">
+            {/* ✅ Grid layout for blogs */}
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+              {blogs.map((blog: Blog) => (
+                <Link
+                  key={blog._id}
+                  href={`/blog/${blog.slug}`}
+                  className="group block overflow-hidden rounded-lg bg-[#2a2a2a] transition-all hover:bg-[#333333] shadow-lg">
+                  <div className="aspect-video w-full overflow-hidden">
+                    <Image
+                      src={blog.image}
+                      alt={blog.title}
+                      width={600}
+                      height={400}
+                      className="h-full w-full object-cover transition-transform group-hover:scale-105"
+                    />
+                  </div>
+                  <div className="p-4">
+                    <h4 className="mb-2 text-2xl font-semibold text-white group-hover:text-orange-400 transition-colors">
+                      {blog.title}
+                    </h4>
+                    <p className="text-xl text-gray-300">{blog.description}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+            {/* ✅ End Grid */}
+          </div>
         </div>
       </div>
       <Footer />
