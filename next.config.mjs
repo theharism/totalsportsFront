9bf33823a0dcf637879219c76dd45a473a3e914c
theharism <chharis9999@gmail.com>
2025-09-11 11:24:19 +0500

    urls redirect

--- next.config.mjs
+++ next.config.mjs
@@ -47,6 +47,76 @@ const nextConfig = {
         source: '/blog/uploads/:path*',
         destination: 'https://dashgenius.space/uploads/:path*',
       },
+      {
+        source: '/soccer',
+        destination: '/streams/soccer',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/cricket',
+        destination: '/streams/cricket',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/motogp',
+        destination: '/streams/motogp',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/f1',
+        destination: '/streams/f1',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/nba',
+        destination: '/streams/nba',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/nfl',
+        destination: '/streams/nfl',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/mlb',
+        destination: '/streams/mlb',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/nhl',
+        destination: '/streams/nhl',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/ufc',
+        destination: '/streams/ufc',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/wwe',
+        destination: '/streams/wwe',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/boxing',
+        destination: '/streams/boxing',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/tennis',
+        destination: '/streams/tennis',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/rugby',
+        destination: '/streams/rugby',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/blog',
+        destination: '/streams/blog',
+        permanent: true, // 301 redirect
+      },
     ];
   },
 }
