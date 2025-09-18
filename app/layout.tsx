97fce9dd436bd3233c8af86343a4b1df944369cd
theharism <chharis9999@gmail.com>
2025-09-18 23:57:50 +0500

    adcash first

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,26 +20,18 @@ export default function RootLayout({
         <script
           type="text/javascript"
           src="//totalsportek.world/aiman.js"
-          defer
         ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              aclib.runPop({zoneId: '10314354'});
-            `,
-          }}
-          defer
-        />
+        
         {/* adcash end*/}
 
         {/* monetag start*/}
-        <meta name="monetag" content="4f46dc0cea68c7fd9b4c5afcf7575e56"></meta>
+        {/* <meta name="monetag" content="4f46dc0cea68c7fd9b4c5afcf7575e56"></meta>
         <script
           src="https://fpyf8.com/88/tag.min.js"
           data-zone="171473"
           data-cfasync="false"
           defer
-        ></script>
+        ></script> */}
         {/* monetag end*/}
 
         {/* schema start*/}
@@ -62,6 +54,21 @@ export default function RootLayout({
             }}
           />
         {/* schema end*/}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              aclib.runPop({ zoneId: '10314354' });
+
+              // Always load Monetag AFTER Adcash
+              const monetag = document.createElement("script");
+              monetag.src = "https://fpyf8.com/88/tag.min.js";
+              monetag.async = true;
+              monetag.setAttribute("data-zone", "171473");
+              monetag.setAttribute("data-cfasync", "false");
+              document.head.appendChild(monetag);
+            `,
+          }}
+        />
       </head>
       <body>
         <div className="relative overflow-x-hidden">{children}</div>
