9f0ba41d26e5efea3038a19edd4c2595a7e804b6
theharism <chharis9999@gmail.com>
2025-09-29 11:15:40 +0500

    updated robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/*?_rsc=",
+      },
+    ],
     sitemap: "https://totalsportek.world/sitemap.xml",
   };
 }
